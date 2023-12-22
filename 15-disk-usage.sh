1e72f3d73efb7868f17377dd056a4a6a61bee84a
awsdevopsonline <punnam.elite@gmail.com>
2023-12-22 21:14:48 +0530

    ifs commit

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -9,9 +9,9 @@ do
 
 
 usage=$(echo $line | awk '{print $6F}' |cut -d % -f1)
-partition=$(echo $line | awd '{print $1F}') 
+partition=$(echo $line | awk '{print $1F}') 
 
-if [ usage -gt $DISK_THRESHOLD ]
+if [ $usage -gt $DISK_THRESHOLD ]
 then
 message+="High Disk Usage $partition : $usage"
 fi
