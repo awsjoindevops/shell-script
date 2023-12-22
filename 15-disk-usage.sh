38efbfbd783e88f5f35fa6d49b56f5ea31ef6c29
awsdevopsonline <punnam.elite@gmail.com>
2023-12-22 22:08:44 +0530

    ifs commit

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -20,4 +20,4 @@ echo -e "message  :  $message"
 
 #echo $message | mail -s "HIGH DISK USAGE" punnamgoud.b23@gmail.com
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "punnamgoud.b23@gmail.com" "ALERT High Disk Usage"
